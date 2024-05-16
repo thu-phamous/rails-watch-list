f3b401db644bfad187f151f414e9e245d1f44f28
Thu Pham <ptthu@gmx.de>
2024-05-16 17:51:36 +0200

    connect the models with each other

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,9 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks
+  has_many :lists
+
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
+  #before_destroy :check_for_bookmarks
+
 end
